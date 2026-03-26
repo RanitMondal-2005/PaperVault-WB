74784d028094ca65a4dc8d2c065aa65d8a206b77
Ranit Mondal <mondalranit2002@gmail.com>
2026-03-26 17:40:39 +0530

    Feat: AI Analyzer Comming Soon

--- wb_papers/users/urls.py
+++ wb_papers/users/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    # AI Analyzer 
+    path('ai-analyzer/', views.ai_analyzer, name='ai_analyzer'),
+    # Login/Log out Logic
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
