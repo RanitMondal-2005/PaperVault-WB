1611943d665f7c1422f5182bf90937df77e1016c
Ranit Mondal <mondalranit2002@gmail.com>
2026-04-03 16:45:55 +0530

    Fix:Minor Improvements made

--- wb_papers/papers/urls.py
+++ wb_papers/papers/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('search/', views.paper_search, name='papers'),
     path('upload/', views.upload_paper, name='upload'),
+    path('paper/edit/<int:pk>/', views.edit_paper, name='edit_paper'),
+    path('paper/delete/<int:pk>/', views.delete_paper, name='delete_paper'),
 ]
