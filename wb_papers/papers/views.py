bf99ec980245ce9090f39f755e99252033980899
Ranit Mondal <mondalranit2002@gmail.com>
2026-04-03 16:44:43 +0530

    Feat:Faculty Paper Edit and Delete logic added in views

--- wb_papers/papers/views.py
+++ wb_papers/papers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Paper
 from .forms import PaperUploadForm 
 from colleges.models import College, Stream
@@ -56,3 +56,49 @@ def upload_paper(request):
     else:
         messages.warning(request, "Verification Pending: Access Denied.")
         return redirect('dashboard')
+    
+# Implementing Papers Edit and Delete Logic
+@login_required
+def edit_paper(request, pk):  
+    # pk is Primary Key / id , to get the exact row where we will update
+    paper = get_object_or_404(Paper, pk=pk)
+    
+    if paper.uploaded_by != request.user:
+        messages.error(request, "Unauthorized access.")
+        return redirect('papers')
+
+    if request.method == 'POST':
+        # NOTE -> Without instance: You have to manually write-> paper.title = request.POST.get('title') and for all other feilds
+        form = PaperUploadForm(request.POST, request.FILES, instance=paper)
+        # NOTE -> FOR NOW : UPLOADING PDF IS ALSO COMPULSORY -> LATER WE WILL UPDATE IT
+        # HACK: If Faculty didn't upload a new pdf file, keep the old one i.e, Updating PDF is optional During Edits
+        if not request.FILES.get('pdf_file'):
+            form.fields['pdf_file'].required = False
+        # Check Forms validation    
+        if form.is_valid():
+            form.save() # save changes to DB
+            messages.success(request, f"Changes saved for {paper.subject_code}!")
+            return redirect('dashboard')
+    else:
+        form = PaperUploadForm(instance=paper)
+        # On GET request, the file isn't mandatory to see the page
+        # Even if Faculty has those prev uploaded papers in diff device then also they can change/view 
+        form.fields['pdf_file'].required = False
+    
+    return render(request, 'edit_paper.html', {'form': form, 'paper': paper})
+
+@login_required
+def delete_paper(request, pk):
+    paper = get_object_or_404(Paper, pk=pk)
+    
+    # Security Check
+    if paper.uploaded_by != request.user:
+        messages.error(request, "Access Denied: You cannot delete this paper.")
+        return redirect('papers')
+
+    if request.method == 'POST':
+        paper.delete()
+        messages.success(request, "Paper has been permanently removed.")
+        return redirect('papers')
+    
+    return render(request, 'delete_paper.html', {'paper': paper})
